64e77fc3e520d0d0247edc66ddf2105765e035d8
vishnukulkarni627 <vishnukulkarni627@gmail.com>
2022-05-11 09:53:16 +0530

    selenium_pom

--- broken_link/bk.py
+++ broken_link/bk.py
@@ -6,10 +6,17 @@ driver = Chrome("./chromedriver.exe")
 driver.get("file:///C:/Users/vishn/Downloads/demo-html/links.html")
 links = driver.find_elements_by_xpath("//a")
 urls =[]
+broken_links=[]
 for link in links:
-    urls.append(link.get_attribute("href"))
+    urls.append((link.text, link.get_attribute("href")))
 for url in urls:
-    response = request("GET",url)
+    response = request("GET",url[1])
     if response.status_code !=200:
-        print(f"The link {url} is broken")
-        sleep(1)
+        # print(f"The link {url} is broken")
+        # sleep(1)
+        broken_links.append(url)
+
+with open('broken_link.txt','w') as f:
+    for item in broken_links:
+        f.write(":".join(item))
+        f.write("\n")
